c389059aa23ae5b574eba4cb34ca45c4690c541d
MohammedBakr44 <mohammed.mobile.tricks@gmail.com>
2021-03-05 05:13:39 +0200

    st added, hope it works this time

---
+++ st/iceberg.h
@@ -0,0 +1,23 @@
+/* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+  /* 8 normal colors */
+  "#161821", /* black   */
+  "#e27878", /* red     */
+  "#b4be82", /* green   */
+  "#e2a478", /* yellow  */
+  "#84a0c6", /* blue    */
+  "#a093c7", /* magenta */
+  "#89b8c2", /* cyan    */
+  "#c6c8d1", /* white   */
+
+  /* 8 bright colors */
+  "#6b7089", /* black   */
+  "#e98989", /* red     */
+  "#c0ca8e", /* green   */
+  "#e9b189", /* yellow  */
+  "#91acd1", /* blue    */
+  "#ada0d3", /* magenta */
+  "#95c4ce", /* cyan    */
+  "#d2d4de", /* white   */
+};
+
